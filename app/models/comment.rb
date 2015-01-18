0741a9fd9a9f20ca01b0715e89fd665edec525e2
Jeff Knox <knoxjeffrey@outlook.com>
2015-01-18 01:19:43 +0000

    vote model, table and associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   belongs_to :post
+  has_many :votes, as: :voteables
   
   validates :body, presence: true
 end
