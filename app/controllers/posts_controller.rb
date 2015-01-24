29fba641233bbfa0e63fdadf31e2e0dc1fd266f5
Jeff Knox <knoxjeffrey@outlook.com>
2015-01-24 00:38:12 +0000

    endpoints exposed for posts index in json and xml

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,12 @@ class PostsController < ApplicationController
   
   def index
     @posts = Post.all.sort_by {|x| x.total_votes}.reverse
+    
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts }
+      format.xml { render xml: @posts }
+    end
   end
   
   def new
